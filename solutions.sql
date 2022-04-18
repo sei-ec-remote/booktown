9262f9d5fd007257c57a791eafd13ae542a8ab9a
patrickga <patrick.g.alvarez@gmail.com>
2022-04-17 19:53:27 -0500

    Where completed

--- solutions.sql
+++ solutions.sql
@@ -5,8 +5,11 @@
         SELECT * FROM subjects ORDER BY location;
 -- ### Where
 -- 3. Find the book "Little Women"
+    SELECT * FROM books WHERE books.title = 'Little Women';
 -- 4. Find all books containing the word "Python"
+    SELECT * FROM books WHERE books.title LIKE '%Python%';
 -- 5. Find all subjects with the location "Main St" sort them by subject
+     SELECT * FROM subjects WHERE location = 'Main St' ORDER BY subject
 
 
 -- ### Joins
